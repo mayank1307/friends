49658ed818658ed8ce8245bbe6b571401a51b7b1
mayank <mayankku1307@gmail.com>
2019-08-24 11:49:40 +0530

    f

---
+++ build/precache-manifest.7ca394194afd3b169afc66056b1b01ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b088149c9156006a6b8e9b063df4556",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b023e67be1c61bebcf6",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "3ae8af4d09290bd100f6",
+    "url": "/static/css/main.2367680a.chunk.css"
+  },
+  {
+    "revision": "4b023e67be1c61bebcf6",
+    "url": "/static/js/2.388b30db.chunk.js"
+  },
+  {
+    "revision": "3ae8af4d09290bd100f6",
+    "url": "/static/js/main.9ba8f19f.chunk.js"
+  },
+  {
+    "revision": "c351faa3ae8b92543456",
+    "url": "/static/js/runtime~main.0ba65364.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
